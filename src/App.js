167fe5cd36abbb6d9113836606de464c53c5495d
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-01-15 19:56:24 +0500

    searchgithub

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Navbar/>
       </div>
     );
   }
