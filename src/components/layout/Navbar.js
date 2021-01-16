f33ac0147254b79a3f92e657682396cc0e35bdab
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-01-16 21:09:06 +0500

    searchgithub

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 
-export class Navbar extends Component {
-    static defaultProps = {
-        title:"Github Search",
-        icon:"fab fa-github"
-    };
-
-    static propTypes ={
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
-    render() {
+const Navbar = props=>{
         return (
-            <nav className="navbar bg-primary">
+            <nav className="navbar bg-primary" style={{boxShadow: '4px 3px 10px #888888'}}>
                 <h1>
-                    <i className={this.props.icon}></i> {this.props.title} 
+                    <i className={props.icon}></i> {props.title} 
                 </h1>
                 
             </nav>
         )
-    }
 }
+Navbar.defaultProps = {
+    title:"Github Search",
+    icon:"fab fa-github"
+};
+
+Navbar.propTypes ={
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
 
 export default Navbar
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
 
-class UserItem extends Component {
-    render() {
+
+const UserItem = (props)=>{
+        const { login, avatar_url, html_url} = props.user;
+        
         return (
-            <div>
-                
+            <div className="card text-center" style={{
+                borderRadius: '5px',
+                boxShadow: '5px 5px 10px #888888',
+                border: 'none'
+            }}>
+                <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}}/>
+                <h3>{login}</h3>
+
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1" style={{borderRadius: '7px'}}>More</a>
+                </div>
             </div>
         )
-    }
 }
-
 export default UserItem
