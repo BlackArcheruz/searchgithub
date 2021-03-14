7be32ae95f2252049fdfbe5e511cb5af50519bbb
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-03-14 11:06:38 +0500

    update

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Alert = ({alert}) => {
+const Alert = ({alert, onClick}) => {
 
     return (
         alert !== null && (
             <div className={`alert alert-${alert.type}`}>
-                <i className="fas fa-info-circle"/> {alert.msg}
+                <i className="fas fa-info-circle"/> {alert.msg} <button className='btn-x' onClick={onClick} style={{border: 'none',outline: 'none'}}><i className="fas fa-times"></i></button>
             </div>
         )
     )
